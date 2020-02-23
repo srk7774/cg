368bd3bb7577771937759fb4eaa5c59354c3913a
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-02-23 13:07:30 +0530

    Update mj3.1.js

--- mj3.1.js
+++ mj3.1.js
@@ -24,6 +24,6 @@ window.MathJax = {
 })();
 
 //addtoany
-(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = false; aa.src = 'http://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = false; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
 
 (function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://www.googletagmanager.com/gtag/js?id=UA-35192647-1'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();

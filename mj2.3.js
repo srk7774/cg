f82f239080081b99f2240eaa32e1f81849453f38
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-02-24 20:58:08 +0530

    Add files via upload

---
+++ mj2.3.js
@@ -0,0 +1,21 @@
+window.MathJax = {
+"fast-preview": {disabled: true},
+  tex2jax: {preview: "none"},
+    CommonHTML: {
+        styles: {'.mjx-chtml': {padding: '6px 0'}},
+		matchFontHeight: false,
+        scale: 110,
+    },
+    tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]},
+    messageStyle: "none",
+    showMathMenu: false,
+  };
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = false; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://www.googletagmanager.com/gtag/js?id=UA-35192647-1'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();

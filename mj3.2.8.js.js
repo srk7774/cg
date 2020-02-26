34cc427c42dbb7111dac9bfbba04bb48228f7243
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-02-26 10:55:24 +0530

    Add files via upload

---
+++ mj3.2.8.js.js
@@ -0,0 +1,33 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$","$"],["\\(","\\)"]]
+  },
+  chtml: {
+    matchFontHeight: false,
+    scale: 1.1
+  },
+  options: {
+    renderActions: {
+      addMenu: [],
+      checkLoading: []
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
+
+// Dynamic DOM insertion of a deferred script (script4.js)
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.defer = "defer";
+  script.src  = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
+//addtoany
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.defer = "defer"; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');

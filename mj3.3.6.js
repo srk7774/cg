e567842524d7e88f21f503ab0bdc898ab337e733
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-03-01 21:08:38 +0530

    Add files via upload

---
+++ mj3.3.6.js
@@ -0,0 +1,26 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$","$"],["\\(","\\)"]]
+  },
+  chtml: {
+    matchFontHeight: false,
+    scale: 1.1
+  },
+  options: {
+    renderActions: {
+      addMenu: [],
+      checkLoading: []
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
+
+// Dynamic DOM insertion of a deferred script (script4.js)
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.defer = "defer";
+  script.src  = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();

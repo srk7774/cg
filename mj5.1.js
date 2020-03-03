2204cd91e835b10a54fde744c87b79490e153eaf
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-03-03 12:58:28 +0530

    Add files via upload

---
+++ mj5.1.js
@@ -0,0 +1,4 @@
+//addtoany
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.defer = "defer"; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.defer = "defer"; po.src = 'https://www.googletagmanager.com/gtag/js?id=UA-35192647-1'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();

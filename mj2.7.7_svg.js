4d3358f5603f792561d8352334e8856370d9d75c
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-03-04 22:17:16 +0530

    Add files via upload

---
+++ mj2.7.7_svg.js
@@ -0,0 +1,24 @@
+window.MathJax = {
+  SVG: {scale: 90},
+  jax: ["input/TeX","output/SVG", "output/PreviewHTML"],
+tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]},
+messageStyle: "none",
+});
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.defer = "defer"; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35192647-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();

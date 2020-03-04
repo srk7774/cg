bf34f3d12add8e95ee1d9f6370fc503ac71c4413
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-03-04 10:53:39 +0530

    Add files via upload

---
+++ mj2.7.7.js
@@ -0,0 +1,32 @@
+window.MathJax = {
+  tex: {
+  window.MathJax = {
+"fast-preview": {disabled: true},
+  tex2jax: {preview: "none"},
+    CommonHTML: {
+        styles: {'.mjx-chtml': {padding: '6px 0'}},
+		matchFontHeight: false,
+        scale: 110,
+    },
+    tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]},
+    messageStyle: "none",
+    showMathMenu: false,
+  };
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS_CHTML";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.defer = "defer"; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35192647-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();

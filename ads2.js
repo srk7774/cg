e71458b37553be1c462cc7d6f4cb881160c39cdb
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-03-06 14:24:39 +0530

    Add files via upload

---
+++ ads2.js
@@ -0,0 +1,3 @@
+(function() { var ad = document.createElement('script'); ad.type = 'text/javascript'; ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+(adsbygoogle = window.adsbygoogle || []).push({});//this is for the first adsense
+(adsbygoogle = window.adsbygoogle || []).push({});

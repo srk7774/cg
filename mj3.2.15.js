4e7716e848f7f39553bb1a8d4f0b66bd943f533e
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-03-09 22:43:37 +0530

    Add files via upload

---
+++ mj3.2.15.js
@@ -0,0 +1,51 @@
+(function () {
+  const onloadPromise = new Promise((resolve, reject) => {
+    window.onload = () => setTimeout(resolve, 500);
+  });
+  window.MathJax = {
+    startup: {
+      pageReady() {
+        onloadPromise.then(() => MathJax.startup.defaultPageReady());
+      }
+    }
+  }
+})();
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["$","$"],["\\(","\\)"]]
+  },
+  chtml: {
+    matchFontHeight: false,
+    scale: 1.1
+  },
+  options: {
+    renderActions: {
+      addMenu: [],
+      checkLoading: []
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
+
+
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.defer = "defer";
+  script.src  = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.defer = "defer"; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35192647-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();

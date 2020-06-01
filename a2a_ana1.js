33b6a4f5fde2edde4c3ceffa60c030b4ba5e2728
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-06-01 12:56:32 +0530

    Add files via upload

---
+++ a2a_ana1.js
@@ -0,0 +1,11 @@
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.defer = "defer"; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35192647-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();

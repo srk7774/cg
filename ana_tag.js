cd659965160a49690fe9c6c2499c3ac9a2053168
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-12-13 23:22:00 +0530

    Add files via upload

---
+++ ana_tag.js
@@ -0,0 +1,9 @@
+(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://www.googletagmanager.com/gtag/js?id=UA-35192647-1'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
+
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = true; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-35192647-1');

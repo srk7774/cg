950a8deced820c2637677cfe96db7a09fef49c47
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-12-27 20:56:48 +0530

    Add files via upload

---
+++ ad_a2a.js
@@ -0,0 +1,3 @@
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = true; aa.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = true; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
--- ana.js
+++ ana.js
@@ -1,9 +1,6 @@
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-35192647-1']);
-  _gaq.push(['_trackPageview']);
+(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://www.googletagmanager.com/gtag/js?id=UA-35192647-1'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  gtag('config', 'UA-35192647-1');

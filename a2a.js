3d11a3edeffcbf825bffae116d115440cb54e6b6
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-12-28 15:26:05 +0530

    Create a2a.js

---
+++ a2a.js
@@ -0,0 +1,1 @@
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = true; aa.src = 'https://static.addtoany.com/menu/page.js'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();

10e65b7c3559761aebcda7f507c1e11feafe6415
srk7774 <Ramakrishnasalagrama@gmail.com>
2020-12-28 18:02:46 +0530

    Add files via upload

---
+++ mj2.7.9_chtml.js
@@ -0,0 +1,21 @@
+window.MathJax = {
+"fast-preview": {disabled: true},
+  tex2jax: {preview: "none"},
+    CommonHTML: {
+        styles: {'.mjx-chtml': {padding: '6px 0'}},
+		matchFontHeight: false,
+        scale: 110,
+EqnChunk: 10,
+EqnChunkFactor: 1.5,
+EqnChunkDelay: 200,
+    },
+    tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]},
+    messageStyle: "none",
+    showMathMenu: false,
+  };
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-AMS_CHTML";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();

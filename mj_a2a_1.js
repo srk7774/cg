7a2c82c0a3021816540fa69dd8dce03c086e6695
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-01-05 11:35:46 +0530

    Create mj_a2a_1.js

---
+++ mj_a2a_1.js
@@ -0,0 +1,12 @@
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
+(function() { 
+var aa = document.createElement('script'); 
+aa.type = 'text/javascript'; aa.async = true; 
+aa.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; 
+var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();

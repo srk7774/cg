f8996a3b2b3470269852804f1759f7acdd7fd83f
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-01-06 21:40:40 +0530

    Create check-for-tex_a2a1.js

---
+++ check-for-tex_a2a1.js
@@ -0,0 +1,22 @@
+(function () {
+  var body = document.body.textContent;
+  if (body.match(/(?:\$|\\\(|\\\[|\\begin\{.*?})/)) {
+    if (!window.MathJax) {
+      window.MathJax = {
+        tex: {
+          inlineMath: {'[+]': [['$', '$']]}
+        }
+      };
+    }
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+    document.head.appendChild(script);
+  }
+})();
+(function() { 
+var aa = document.createElement('script'); 
+aa.type = 'text/javascript'; 
+aa.async = true; 
+aa.src = 'https://static.addtoany.com/menu/page.js'; 
+var b = document.getElementsByTagName('script')[0]; 
+b.parentNode.insertBefore(aa, b); })();

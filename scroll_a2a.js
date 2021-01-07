c57394e73ea98b32f7c89acb29fde94c320f20a9
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-01-07 20:22:56 +0530

    Create scroll_a2a.js

---
+++ scroll_a2a.js
@@ -0,0 +1,2 @@
+var la=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===la||0!=document.body.scrollTop&&!1===la)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://static.addtoany.com/menu/page.js";
+var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),la=!0)},!0);

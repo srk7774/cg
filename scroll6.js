32d1b1fb6c8eff72d7ac0eec1a4569c5052467f3
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-03-14 15:49:37 +0530

    Create scroll6.js

---
+++ scroll6.js
@@ -0,0 +1,8 @@
+(function() { 
+var aa = document.createElement('script'); 
+aa.type = 'text/javascript'; 
+aa.async = true; 
+aa.src = 'https://static.addtoany.com/menu/page.js'; 
+var b = document.getElementsByTagName('script')[0]; 
+b.parentNode.insertBefore(aa, b); })();
+var la=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===la||0!=document.body.scrollTop&&!1===la)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.jsdelivr.net/gh/srk7774/cg/mj2.7.9_svg.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),la=!0)},!0);

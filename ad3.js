463ea70108e7e2e3ce79f9527909d02a630c5621
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-07-28 10:20:07 +0530

    Add files via upload

---
+++ ad3.js
@@ -0,0 +1,2 @@
+(function() { var ad = document.createElement('script'); ad.type = 'text/javascript'; ad.async = true; ad.crossorigin="anonymous"; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3975800393524684'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+

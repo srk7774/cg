6ebe94920d7af37a6c18b599d58762f49888505e
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-03 11:05:21 +0530

    Create mj3_svg_3.js

---
+++ mj3_svg_3.js
@@ -0,0 +1,23 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$","$"],["\\(","\\)"]]
+  },
+  svg: {
+    matchFontHeight: false,
+    scale: 0.9
+  },
+  options: {
+    renderActions: {
+      addMenu: [],
+      checkLoading: []
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  },
+};
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();

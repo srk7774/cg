0006dd46e7fa87246888525ff19c872fa895b589
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-04 18:45:27 +0530

    Update and rename mj2.7.9_3.js to mj2.7.9_4.js

--- mj2.7.9_4.js
+++ mj2.7.9_4.js
@@ -1,5 +1,5 @@
 window.MathJax = {
-"fast-preview": {disabled: true},
+"fast-preview": {disabled: false},
   tex2jax: {preview: "none"},
     CommonHTML: {
         styles: {'.mjx-chtml': {padding: '6px 0'}},

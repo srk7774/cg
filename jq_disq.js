7b48d6154456744173c245269cf99a83720b2f4e
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-09 11:47:53 +0530

    Update and rename cash_disq.js to jq_disq.js

--- jq_disq.js
+++ jq_disq.js
@@ -1,5 +1,5 @@
                 var element6 = document.createElement(&quot;script&quot;);
-element6.src = &quot;https://cdnjs.cloudflare.com/ajax/libs/cash/8.1.0/cash.min.js&quot;;
+element6.src = &quot;https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js&quot;;
 document.body.appendChild(element6);
 
 (function($){
@@ -11,4 +11,4 @@ document.body.appendChild(element6);
             }
         });
     }, 300);
-})(cash);
+})(jQuery);

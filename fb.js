75f2ef9709673b4036a73c1db67aa2175a83e7b8
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-18 20:41:15 +0530

    Update and rename fb.js to fb1.js

--- fb.js
+++
@@ -1,2 +0,0 @@
-(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = true; 
-aa.defer=true; aa.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&amp;version=v12.0&amp;appId=380546885348892&amp;autoLogAppEvents=1'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();

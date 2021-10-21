4b356b6f2459f98391614442b3d487c151ab9f0f
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-21 17:22:54 +0530

    Create block1.js

---
+++ block1.js
@@ -0,0 +1,25 @@
+var jQInit = jQInit || [];
+
+jQInit.push( [ 'myModule1', function( $ ) {
+
+	$(window).on('load', function () {
+    $(&quot;iframe[id^='dsq-app']&quot;).each(function (i, el) {
+        if(i===0 || i===2) {
+            el.remove();
+        };
+    });
+        });
+    const disqus = jQuery('#disqus_thread');
+
+disqus.ready(function() {
+  setTimeout(function() {
+    if (disqus.children().length &gt;= 2) {
+      const comments = disqus.find('iframe:nth-child(2)').detach();
+      disqus.empty().append(comments);
+    }
+  }, 2000);
+});
+};
+
+document.body.appendChild(myScript);
+	} ] );

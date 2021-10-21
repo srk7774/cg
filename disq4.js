0327e716e6b3e18912f9181c131bdf759f4268b0
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-22 00:00:46 +0530

    Create disq4.js

---
+++ disq4.js
@@ -0,0 +1,21 @@
+<![CDATA[    
+var disqus_config = function () {
+        // Replace PAGE_URL with your page's canonical URL variable
+        this.page.url = <data:blog.canonicalUrl/>;  
+        
+        // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        this.page.identifier = <data:blog.pageId/>; 
+    };
+    
+    (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+        var d = document, s = d.createElement('script');
+      s.defer = true;
+        
+        // IMPORTANT: Replace EXAMPLE with your forum shortname!
+        s.src = 'https://campusgate.disqus.com/embed.js';
+        
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+  var disqus_url = '<data:blog.canonicalUrl/>'; if (!disqus_url.length) { disqus_url = '<data:blog.url/>'; }
+]]> 

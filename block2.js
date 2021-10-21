9d6f21eb60635adddec918120c062a3b34c15a66
srk7774 <Ramakrishnasalagrama@gmail.com>
2021-10-22 00:27:31 +0530

    Create block2.js

---
+++ block2.js
@@ -0,0 +1,17 @@
+$(window).on('load', function () {
+    $(&quot;iframe[id^='dsq-app']&quot;).each(function (i, el) {
+        if(i===0 || i===2) {
+            el.remove();
+        };
+    });
+        });
+    const disqus = jQuery('#disqus_thread');
+
+disqus.ready(function() {
+  setTimeout(function() {
+    if (disqus.children().length &gt;= 2) {
+      const comments = disqus.find('iframe:nth-child(2)').detach();
+      disqus.empty().append(comments);
+    }
+  }, 2000);
+});

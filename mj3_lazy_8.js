dd847ca7fe84280191a5bb1b5e42400a36cdbc91
srk7774 <ramakrishna.salagrama@gmail.com>
2022-10-30 13:18:44 +0530

    Create mj3_lazy_8.js

---
+++ mj3_lazy_8.js
@@ -0,0 +1,41 @@
+window.MathJax = {
+startup: {
+	typeset: false,
+  },
+loader: {load: ['ui/lazy']},
+  tex: {
+    inlineMath: [["$","$"],["\\(","\\)"]]
+  },
+  svg: {
+    matchFontHeight: false,
+    scale: 0.9
+  },
+  options: {
+    renderActions: {
+      addMenu: [],
+      checkLoading: [],
+      lazyMargin: '300px',
+      lazyAlwaysTypeset: null
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  },
+  startup: {
+    ready: function () {
+      var SVG = MathJax._.output.svg_ts.SVG;
+      var percent = MathJax._.util.lengths.percent;
+      SVG.prototype.setScale = function (node) {
+        if (this.options.scale !== 1) {
+          this.adaptor.setStyle(node, 'fontSize', percent(this.options.scale));
+        }
+      }
+      MathJax.startup.defaultReady();
+    }
+  }
+};
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();

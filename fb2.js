84dc1f66d4da3ebba946a2df8c1f900b2452565d
srk7774 <ramakrishna.salagrama@gmail.com>
2022-11-02 21:30:59 +0530

    Update and rename fb1.js to fb2.js

--- fb2.js
+++ fb2.js
@@ -2,8 +2,8 @@ function() {
   var aa = document.createElement('script'); aa.type = 'text/javascript'; 
   aa.async = true; 
   aa.crossorigin='anonymous'
-  aa.nonce='afchAxmB';
-  aa.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&amp;version=v12.0&amp;appId=380546885348892&amp;autoLogAppEvents=1'; 
+  aa.nonce='qOU7Qjbj';
+  aa.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&amp;version=v15.0&amp;appId=380546885348892&amp;autoLogAppEvents=1'; 
   var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); 
 }();
 

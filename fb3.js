a64c6d2fefd5f94aa4751ce8cf2774661a6e2951
srk7774 <ramakrishna.salagrama@gmail.com>
2022-11-03 14:07:41 +0530

    Update and rename fb2.js to fb3.js

---
+++ fb3.js
@@ -0,0 +1,3 @@
+(function() { var aa = document.createElement('script'); aa.type = 'text/javascript'; aa.async = true; aa.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v15.0&appId=380546885348892&amp;autoLogAppEvents=1'; var b = document.getElementsByTagName('script')[0]; b.parentNode.insertBefore(aa, b); })();
+
+

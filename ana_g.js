20c75508b5fb6d5261dc4163f460dcc4fa3b8f9b
srk7774 <ramakrishna.salagrama@gmail.com>
2023-08-05 23:45:02 +0530

    ana_g.js

---
+++ ana_g.js
@@ -0,0 +1,6 @@
+(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://www.googletagmanager.com/gtag/js?id=G-JFWQ5R01VV'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JFWQ5R01VV');

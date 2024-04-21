0b37bcbc0b0daa3db042e845da28d5887dd8fe0f
Ramakrishna Salagrama <ramakrishna.salagrama@gmail.com>
2024-04-21 12:08:46 +0530

    Create mj_210424.js

---
+++ mj_210424.js
@@ -0,0 +1,23 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$","$"],["\\(","\\)"]]
+  },
+  chtml: {
+    matchFontHeight: true,
+    scale: 1.0
+  },
+  options: {
+    renderActions: {
+      addMenu: [],
+      checkLoading: []
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process'
+  }
+};
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
